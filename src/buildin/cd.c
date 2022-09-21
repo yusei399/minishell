78fe5c66a3c8ad055c0191789bf5c708f727a355
yusei399 <yusei2021@keio.jp>
2022-09-21 20:56:55 +0900

    cdの途中

--- src/buildin/cd.c
+++ src/buildin/cd.c
@@ -16,6 +16,25 @@ void	error_msg(char **argv)
 	ft_putendl_fd(argv[1], 2);
 }
 
+char	*get_env_path(t_env *env, const char *var, size_t len)
+{
+	char	*old_path;
+	int		i;
+	int		j;
+	int		malloc_len;
+
+	while (env && env->next != NULL)
+	{
+		if (ft_strncmp(env->value, var, len) == 0)
+		{
+			malloc_len = ft_strlen(env->value) - len;
+			if (!(old_path = malloc(sizeof(char) * malloc_len + 1)))
+				return (NULL);
+			
+		}
+	}
+}
+
 void	ft_cd(char	*path)
 {
 	chdir(path);
