21525e9ba737b568a2e6ac2c0ea99a731f12bceb
susui <skymattya@gmail.com>
2022-11-12 15:48:34 +0900

    pipe split DONE

--- src/lexer/pipe_split.c
+++ src/lexer/pipe_split.c
@@ -9,12 +9,20 @@ int is_pipe(char c)
 	return (0);
 }
 
-//int	pipe_len();
-//ft_strndup();
+size_t	pipe_len(char *arg)
+{
+	size_t	i;
+
+	i = 0;
+	while(arg[i] != '|' && arg[i])
+		i++;
+	return (i);
+}
 char	*extract_pipe(char *arg, int n)
 {
-	int	pipe_count;
-	int	i;
+	int		pipe_count;
+	int		i;
+	char	*str;
 
 	pipe_count = 0;
 	i = 0;
@@ -27,7 +35,10 @@ char	*extract_pipe(char *arg, int n)
 		i++;
 	}
 	// arg[i]の位置から次のpipeもしくはNULLまでを取る
-//	ft_strndup();
+	str = ft_strndup(&arg[i], pipe_len(&arg[i]));
+	if (!str)
+		return (NULL);
+	return (str);
 }
 int	pipe_init(t_shell *shell, size_t pipe_count)
 {
@@ -56,6 +67,7 @@ int	add_pipe_list(t_shell *shell, size_t pipe_count)
 	size_t	i;
 	t_cmd	*new;
 
+	i = 0;
 	while(i <= pipe_count)
 	{
 		new = lstnew();
@@ -63,8 +75,10 @@ int	add_pipe_list(t_shell *shell, size_t pipe_count)
 			return (1);
 		lstadd_back(&shell->cmd, new);
 		// nullが帰ってきたとき(失敗した時)にfreeしなきゃ
+		printf("list size: %zu\n", i); // todo
 		i++;
 	}
+	printf("\n\n"); // todo
 	return (0);
 }
 
@@ -97,30 +111,40 @@ int 	pipe_split(t_shell *shell)
 	int	p_count;
 
 	p_count = pipe_count(shell);
-	// 0個だったら終わり [ok] ->
-	// todo[] 一個分argを入れる
+	printf("pipe count : %d\n", p_count); //  todo OK
 	if (p_count == -1)
 		return (1);
-	// pipeがある分だけリストを作ってパイプを繋げておく []
-	add_pipe_list(shell, p_count);
+	add_pipe_list(shell, p_count); // todo OKっぽい
 	// lst->cmd に アーギュメントを入れていく
-	pipe_init(shell, p_count);
+	pipe_init(shell, p_count); // todo -[segv]-
 	return (0);
 }
-/*
-int	main()
+
+// /* -----------------------------------------------------------------------
+int	main(int argc, char **argv)
 {
 	t_shell shell;
 	t_cmd *cmd;
 	int i;
 
+	memset(&shell,0,sizeof(t_shell));
+	shell.cmd = lstnew();
+	if (argc == 1)
+		return (1);
 	i = 0;
-	pipe_list(&shell, 1);
 	cmd = shell.cmd;
+	shell.arg = argv[1];
+	pipe_split(&shell);
+	size_t size = 1;
+	printf("Full argument is: %s\n", shell.arg);
 	while (cmd)
 	{
+		printf("%zu arg is : %s\n", size, cmd->command);
+		size++;
 		cmd = cmd->next;
-		i++;
 	}
-	printf("%d", i);
-}*/
+	return (0);
+}
+// ----------------------------------------------------------------------- */
+
+//最後のにNULLが入ってない
