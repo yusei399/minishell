56120852e821de407e5d44a9192e45b0843171f6
yusei399 <yusei2021@keio.jp>
2022-11-28 23:03:31 +0900

    shell_executive

--- src/minishell.c
+++ src/minishell.c
@@ -3,16 +3,34 @@
 	//envpをsplitして前と後ろに分けて線形リストに入れ
 	//後でaccessコマンドで実行して実行できたらする
 		// forkでプロセスを複製して実行する
+void	shell_executive(t_shell *shell)
+{
+	if (lexer(shell))
+	{
+		printf("Invalid arg");
+		free(shell->input);
+	}
+	else
+	{
+		executor(shell);
+
+	}
+}
+
 void	minishell(int argc, char **argv, char **envp)
 {
 	t_shell	shell;
 
+	(void)argv;
+	(void)argc;
 	ft_bzero(&shell, sizeof(t_shell));
 	split_env(&shell, envp);
+	shell = (t_shell){};
+	g_status = 0;
+
 	while (1)
 	{
 		shell.input = readline("minishell>$");
-		split_env(&shell, envp);
 		executor(&shell);
 		//lexerの上にheredoc
 		lexer(&shell);
