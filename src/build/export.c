7f0045e8f53c69989ed066260fec81474410e408
susui <skymattya@gmail.com>
2022-12-02 16:53:01 +0900

    一旦done

--- src/build/export.c
+++ src/build/export.c
@@ -1,7 +1,7 @@
 #include "../../inc/minishell.h"
 
-// export name=value
-// name が同じやつは上書きする
+// export key=value
+// key が同じやつは上書きする
 
 int	check_export(char *arg)
 {
@@ -12,31 +12,31 @@ int	check_export(char *arg)
 	return (0);
 }
 
-char	*name_env(char	*arg)
+char	*key_env(char	*arg)
 {
 	size_t	i;
-	char	*name;
+	char	*key;
 
 	i = 0;
 	while (arg[i++] != '=')
 		;
-	name = malloc(sizeof(char ) * i);
-	if (!name)
+	key = malloc(sizeof(char ) * i);
+	if (!key)
 		return (NULL);
-	ft_memmove(name, arg, i)
-	return (name);
+	ft_memmove(key, arg, i)
+	return (key);
 }
 
 void	ft_export(t_shell *shell, char *arg)
 {
 	t_env	*env;
-	char	*name;
+	char	*key;
 
 	if (check_export(arg))
 		return (NULL);
-	name = name_env(arg);
-	ft_unset(shell, name);
-	free(name);
+	key = key_env(arg);
+	ft_unset(shell, key);
+	free(key);
 	// ここらへんの関数の使い方確認頼む
 	env = new_env(arg);
 	add_back_env(shell->env, env);
---
+++ src/build/unset.c
@@ -0,0 +1,48 @@
+#include "../../inc/minishell.h"
+
+static int is_exact_match(char *input, char *end)
+{
+	if (ft_strlen(input) != ft_strlen(end))
+		return (0);
+	if (ft_strncmp(input, end, ft_strlen(end)) != 0)
+		return (0);
+	return (1);
+}
+
+void	clean_key(t_shell *shell, t_env *env)
+{
+	t_env	tmp;
+
+	if (!env)
+	{
+		tmp = env;
+		shell->env = env->next;
+	}
+	else
+	{
+		tmp = env->next;
+		env->next = tmp->next;
+	}
+	free(tmp->key);
+	free(tmp->val);
+	free(tmp);
+}
+
+void	ft_port(t_shell *shell, char *key)
+{
+	t_env	*env;
+	t_env	*pre;
+
+	env = shell->env;
+	pre = NULL;
+	while (env)
+	{
+		if (is_exact_match(env->key, key))
+		{
+			clean_key(shell, pre);
+			return ;
+		}
+		pre = env;
+		env = env->next;
+	}
+}
